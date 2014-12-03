f553b1fc718551728b3690f610fc2c4d6c4636dc
doneallison <doneallison@gmail.com>
2014-12-03 17:44:51 -0500

    implement different behavior for new vs. existing users

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,27 +5,56 @@ class SessionController < ApplicationController
   end
 
   def create
+
     @auth_hash = auth_hash
+
     if session[:is_tutor]
-      @tutor = Tutor.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || Tutor.create_from_omniauth(auth_hash)
-      if @tutor
-           session[:tutor_id] = @tutor.id
-           redirect_to edit_tutor_path(current_tutor)
-           # render 'tutor/edit'
+
+      if Tutor.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+        @tutor = Tutor.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+        redirect_to appointment_index_path
       else
-           redirect_to root_url
+        @tutor = Tutor.create_from_omniauth(auth_hash)
+        session[:tutor_id] = @tutor.id
+        redirect_to edit_tutor_path(current_tutor)
       end
+    
     else
-      @student = Student.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || Student.create_from_omniauth(auth_hash)
-      if @student
-           session[:student_id] = @student.id
-           redirect_to edit_student_path(current_student)
-           # render 'student/edit'
+
+      if Student.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+        @student = Student.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+        redirect_to appointment_index_path
       else
-           redirect_to root_url
+        @student = Student.create_from_omniauth(auth_hash)
+        session[:student_id] = @student.id
+        redirect_to edit_student_path(current_student)
       end
+      
     end
+
   end
+
+  # def create
+  #   @auth_hash = auth_hash
+  #   binding.pry
+  #   if session[:is_tutor]
+  #     @tutor = Tutor.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || Tutor.create_from_omniauth(auth_hash)
+  #     if @tutor
+  #          session[:tutor_id] = @tutor.id
+  #          redirect_to edit_tutor_path(current_tutor)
+  #     else
+  #          redirect_to root_url
+  #     end
+  #   else
+  #     @student = Student.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || Student.create_from_omniauth(auth_hash)
+  #     if @student
+  #          session[:student_id] = @student.id
+  #          redirect_to edit_student_path(current_student)
+  #     else
+  #          redirect_to root_url
+  #     end
+  #   end
+  # end
 
   def destroy
     reset_session
