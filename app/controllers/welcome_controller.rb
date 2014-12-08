5a05e296c02aa9d140137c252e138ff1529e8824
bidah <barto@colaboractive.com>
2014-12-08 14:21:37 -0300

    homepage show new layout when user logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,22 @@
 class WelcomeController < ApplicationController
   layout "welcome_index"
+  before_filter :find_tutor_or_student
   def index
-  	# if logged_in?
-  	# 	redirect_to appointment_index_path
-  	# end
+    if logged_in?
+      render layout: "appointment"
+    else
+      render layout: "welcome_index"
+    end
+  end
+
+  def find_tutor_or_student
+    if current_tutor
+      @tutor = current_tutor
+      @user = current_tutor
+    else
+      @student = current_student
+      @user = current_student
+    end
   end
 end
+
