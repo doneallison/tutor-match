60c6e7ac654221544c2cf562bc1f490d09725ed8
doneallison <doneallison@gmail.com>
2014-12-09 11:18:31 -0500

    fix helper method syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_student, :current_tutor
 
-  helper_method def logged_in?
+  def logged_in?
     !!current_student || !!current_tutor#double negation to convert to boolean
   end
 
-  helper_method def current_student
+  def current_student
     @current_student ||= Student.find(session[:student_id]) if session[:student_id]
   end
 
-  helper_method def current_tutor
+  def current_tutor
     @current_tutor ||= Tutor.find(session[:tutor_id]) if session[:tutor_id]
   end
 
