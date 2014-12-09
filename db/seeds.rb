084d09ab83ba40b574f23da61a08eb7a81d8f71e
doneallison <doneallison@gmail.com>
2014-12-09 16:50:30 -0500

    fix new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Tutor.create(name: "David Heinemeier Hansson")
-Tutor.create(name: "Yukihiro Matsumoto")
-Tutor.create(name: "_why the lucky stiff")
-Tutor.create(name: "Grace Hopper")
-Tutor.create(name: "Ada Lovelace")
-
-Tutor.find(1).skills.push(skill.find(1), skill.find(2), skill.find(3), skill.find(4))
-Tutor.find(1).windows.push(window.find(1), window.find(2), window.find(3))
-
-Tutor.find(2).skills.push(skill.find(3), skill.find(4), skill.find(5), skill.find(6))
-Tutor.find(2).windows.push(window.find(3), window.find(7), window.find(11))
-
-Tutor.find(3).skills.push(skill.find(2), skill.find(7), skill.find(12), skill.find(17))
-Tutor.find(3).windows.push(window.find(5), window.find(6), window.find(10))
-
-Tutor.find(4).skills.push(skill.find(8), skill.find(4), skill.find(13), skill.find(16))
-Tutor.find(4).windows.push(window.find(4), window.find(7), window.find(9))
-
-Tutor.find(5).skills.push(skill.find(1), skill.find(2), skill.find(3), skill.find(7))
-Tutor.find(5).windows.push(window.find(1), window.find(2), window.find(3))
 
 Window.create(name: "Sunday Morning")
 Window.create(name: "Sunday Afternoon")
@@ -56,3 +36,24 @@ Skill.create(name: "Rails")
 Skill.create(name: "JavaScript")
 Skill.create(name: "jQuery")
 Skill.create(name: "Ajax")
+
+Tutor.create(name: "David Heinemeier Hansson")
+Tutor.create(name: "Yukihiro Matsumoto")
+Tutor.create(name: "_why the lucky stiff")
+Tutor.create(name: "Grace Hopper")
+Tutor.create(name: "Ada Lovelace")
+
+Tutor.find(1).skills.push(Skill.find(1), Skill.find(2), Skill.find(3), Skill.find(4))
+Tutor.find(1).windows.push(Window.find(1), Window.find(2), Window.find(3))
+
+Tutor.find(2).skills.push(Skill.find(3), Skill.find(4), Skill.find(5), Skill.find(6))
+Tutor.find(2).windows.push(Window.find(3), Window.find(7), Window.find(11))
+
+Tutor.find(3).skills.push(Skill.find(2), Skill.find(7), Skill.find(12), Skill.find(17))
+Tutor.find(3).windows.push(Window.find(5), Window.find(6), Window.find(10))
+
+Tutor.find(4).skills.push(Skill.find(8), Skill.find(4), Skill.find(13), Skill.find(16))
+Tutor.find(4).windows.push(Window.find(4), Window.find(7), Window.find(9))
+
+Tutor.find(5).skills.push(Skill.find(1), Skill.find(2), Skill.find(3), Skill.find(7))
+Tutor.find(5).windows.push(Window.find(1), Window.find(2), Window.find(3))
